df5d434d28c80929527a525e45efd46accb92b7b
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2025-04-11 10:00:02 -0500

    refactor: :art: add comments on types

--- src/modules/thumbnail/types.ts
+++ src/modules/thumbnail/types.ts
@@ -1,37 +1,124 @@
+/**
+ * Thumbnail Generation Types
+ */
 export type IntervalType = "manual" | "automatic";
 
+/**
+ * Options for thumbnail generation.
+ */
 export interface ThumbnailOptions {
+  /**
+   * The interval between thumbnails in seconds.
+   */
   numSnapshots?: number;
+
+  /**
+   * The format of the thumbnail images.
+   */
   format?: "png" | "jpg";
+
+  /**
+   * The width of the thumbnail images.
+   */
   method?: "auto" | "native" | "ffmpeg";
+
+  /**
+   * The height of the thumbnail images.
+   */
   thumbnailInterval?: number;
+
+  /**
+   * The number of thumbnails to generate.
+   */
   intervalType?: IntervalType;
 }
 
+/**
+ * Interface for thumbnail generation progress.
+ */
 export interface Thumbnail {
+  /**
+   * The URL of the generated thumbnail.
+   */
   blobUrl: string;
+
+  /**
+   * The Blob object of the generated thumbnail.
+   */
   blob: Blob;
+
+  /**
+   * The canvas element used to generate the thumbnail.
+   */
   arrayBuffer: ArrayBuffer;
+
+  /**
+   * The timestamp of the thumbnail in seconds.
+   */
   timestamp: number;
+
+  /**
+   * The width of the thumbnail image.
+   */
   videoDuration: number;
+
+  /**
+   * The height of the thumbnail image.
+   */
   width?: number;
+
+  /**
+   * The height of the thumbnail image.
+   */
   height?: number;
+
+  /**
+   * The format of the thumbnail image.
+   */
   isBlack?: boolean;
+
+  /**
+   * The format of the thumbnail image.
+   */
   method: "native" | "ffmpeg";
 }
 
+/**
+ * Interface for thumbnail generation progress.
+ */
 export interface CapturedFrame {
+  /**
+   * The URL of the generated thumbnail.
+   */
   blob: Blob;
+
+  /**
+   * The Blob object of the generated thumbnail.
+   */
   canvas: HTMLCanvasElement;
+
+  /**
+   * The Blob object of the generated thumbnail.
+   */
   timestamp: number;
+
+  /**
+   * The Blob object of the generated thumbnail.
+   */
   width: number;
+
+  /**
+   * The Blob object of the generated thumbnail.
+   */
   height: number;
 }
 
+/**
+ * Interface for thumbnail generation progress.
+ */
 export type ThumbnailGenerationStatus =
   | "initializing"
   | "generatingThumbnails"
   | "completed"
   | "errorGeneratingThumbnails"
   | "thumbnailsGenerated";
-
--- src/modules/upload/types.ts
+++ src/modules/upload/types.ts
@@ -1,34 +1,107 @@
 import { FastevoUploader } from "@/fastevoUploader";
 import { ThumbnailOptions } from "../thumbnail";
 
+/**
+ * Interface for the upload progress event.
+ */
 export type UploadProgress = {
+  /**
+   * The percentage of the upload completed.
+   */
   percentage: number;
+
+  /**
+   * The number of bytes uploaded so far.
+   */
   uploadedBytes: number;
+
+  /**
+   * The total number of bytes to be uploaded.
+   */
   totalBytes: number;
+
+  /**
+   * The time in milli
+   * seconds since the upload started.
+   */
   speedBps: number;
+
+  /**
+   * The time in milliseconds since the upload started.
+   */
   timeRemaining: number;
 };
 
+/**
+ * Interface for the upload error event.
+ */
 export type UploadRequest = {
+  /**
+   * The token used for multipart upload.
+   */
   multipartUploadToken: string;
+
+  /**
+   * The file to be uploaded.
+   */
   file: File;
+
+  /**
+   * The API URL base for the upload.
+   */
   apiUrlBase?: string;
+
+  /**
+   * The name of the file to be uploaded.
+   */
   generateThumbnails?: boolean;
+
+  /**
+   * Options for generating thumbnails.
+   */
   thumbnailOptions?: ThumbnailOptions;
 };
 
+/**
+ * Interface for the upload result.
+ */
 export type UploadResult = {
+  /**
+   * The URL of the uploaded file.
+   */
   thumbnails: string[];
 };
 
 export type UploadConfig = {
+  /**
+   * The minimum speed in bytes per second.
+   */
   minSpeedBps: number;
+
+  /**
+   * The minimum percentage of the upload completed.
+   */
   bufferPercentage: number;
+
+  /**
+   * The maximum time in milliseconds to wait before retrying.
+   */
   maxTimeout: number;
+
+  /**
+   * The maximum number of retries for the upload.
+   */
   maxRetries: number;
+
+  /**
+   * The delay in milliseconds before retrying the upload.
+   */
   retryDelay: (attempt: number) => number;
 };
 
+/**
+ * Interface for the upload event.
+ */
 export type UploadEvent =
   | "uploadProgress"
   | "uploadComplete"
@@ -38,19 +111,57 @@ export type UploadEvent =
   | "thumbnailsGenerationStarted"
   | "thumbnailsGenerated";
 
+/**
+ * Interface for the upload event listener.
+ */
 export type UploadListener = (...args: any[]) => void;
 
+/**
+ * Interface for the upload manager.
+ */
 export type UploaderConfig = {
+  /**
+   * The token used for multipart upload.
+   */
   uploadToken: string;
+
+  /**
+   * The file to be uploaded.
+   */
   options?: {
+    /**
+     * The file to be uploaded.
+     */
     apiUrlBase?: string;
   };
 };
 
+/**
+ * Interface for the upload manager.
+ */
 export type ActiveUpload = {
+  /**
+   * The file to be uploaded.
+   */
   uploader: FastevoUploader;
+
+  /**
+   * The file to be uploaded.
+   */
   lastUpdateTime: number;
+
+  /**
+   * The file to be uploaded.
+   */
   status: "idle" | "uploading" | "paused" | "resumed";
+
+  /**
+   * The file to be uploaded.
+   */
   lastPauseTime: number;
+
+  /**
+   * The file to be uploaded.
+   */
   totalPausedTime: number;
-}
+};
